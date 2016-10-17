adfdc93fba23bc9e62ad2e58bf6baed7145ee05b
domingogallardo <domingo.gallardo@ua.es>
2016-10-17 13:26:49 +0200

    TIC-26 Refactor fixed bug test claves ajenas

--- test/service/UsuariosServiceTest.java
+++ test/service/UsuariosServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.*;
 import org.dbunit.*;
 import org.dbunit.dataset.*;
 import org.dbunit.dataset.xml.*;
+import org.dbunit.operation.*;
 import java.io.FileInputStream;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
@@ -40,6 +41,18 @@ public class UsuariosServiceTest {
         IDataSet initialDataSet = new FlatXmlDataSetBuilder().build(new
         FileInputStream("test/resources/usuarios_dataset.xml"));
         databaseTester.setDataSet(initialDataSet);
+
+        // Definimos como operación TearDown DELETE_ALL para que se
+        // borren todos los datos de las tablas del dataset
+        // (el valor por defecto DbUnit es DatabaseOperation.NONE)
+        databaseTester.setTearDownOperation(DatabaseOperation.DELETE_ALL);
+
+        // Definimos como operación SetUp CLEAN_INSERT, que hace un
+        // DELETE_ALL de todas las tablase del dataset, seguido por un
+        // INSERT. (http://dbunit.sourceforge.net/components.html)
+        // Es lo que hace DbUnit por defecto, pero así queda más claro.
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+
         databaseTester.onSetup();
     }
 
